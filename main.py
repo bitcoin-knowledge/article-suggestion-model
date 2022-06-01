32177cd8fee533f8f2768d503e031d924c0ae13e
austinkelsay <austinkelsay@yahoo.com>
2022-06-01 18:53:39 -0500

    Adding cors back in

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from article_suggestion import suggest_article
 class Query(BaseModel):
     question: str
 
-# origins = [
-#     "http://localhost:3000",
-#     "https://bitcoin-knowledge-bot.vercel.app",
-#     "https://bitcoin-knowledge-bot-frontend.vercel.app",
-#     "http://localhost:19006",
-#     "http://localhost:19002"
-# ]
+origins = [
+    "http://localhost:3000",
+    "https://bitcoin-knowledge-bot.vercel.app",
+    "https://bitcoin-knowledge-bot-frontend.vercel.app",
+    "http://localhost:19006",
+    "http://localhost:19002"
+]
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_methods=["GET", "POST"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
